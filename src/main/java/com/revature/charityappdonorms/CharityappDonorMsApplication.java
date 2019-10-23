0902075dfd86314852cb8d7aef47bbd81b5a6efe
Naresh Kumar H <nareshkumarh@live.com>
2019-10-23 12:53:56 +0530

    Updated

---
+++ src/main/java/com/revature/charityappdonorms/CharityappDonorMsApplication.java
@@ -0,0 +1,13 @@
+package com.revature.charityappdonorms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharityappDonorMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharityappDonorMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/charityappdonorms/CharityappDonorMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.charityappdonorms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharityappDonorMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
